0b68e6e9771a75fb6c4bd2d028d44d1c8aafe8ec
Z-H-ANG <2415636605@qq.com>
2021-05-21 11:19:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/unittest2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.unittest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+
+
+
+}
